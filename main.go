84ded0a6ddce5dbba5c58ac988ac57a165ef69b9
lucymidgley <lucymidgley13@gmail.com>
2025-08-10 19:00:35 +0900

    add ground

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"image/color"
 	_ "image/png"
 	"io/fs"
+	"math/rand"
 
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/hajimehoshi/ebiten/v2/text"
@@ -19,6 +20,7 @@ const (
 	ScreenHeight = 600
 	BlockHeight  = 64
 	BlockWidth   = 64
+	GroundY      = ScreenHeight - 2*BlockHeight
 )
 
 //go:embed assets/*
@@ -76,19 +78,49 @@ func mustLoadImages(path string) []*ebiten.Image {
 }
 
 var PlayerSprite = mustLoadImage("assets/giraffe.png")
+var BrickSprites = mustLoadImages("assets/bricks/*.png")
 
 type Player struct {
 	game     *Game
 	position Vector
 	sprite   *ebiten.Image
 }
+type Brick struct {
+	position Vector
+	sprite   *ebiten.Image
+}
 
+func NewBrick(x, y float64) *Brick {
+	sprite := BrickSprites[rand.Intn(len(Bricks))]
+
+	pos := Vector{
+		X: x,
+		Y: ScreenHeight - BlockHeight - y,
+	}
+	return &Brick{
+		position: pos,
+		sprite:   sprite,
+	}
+}
+
+func (g *Game) DrawGround() {
+	for i := 0; i < 13; i++ {
+		brick := NewBrick(float64(i*BlockWidth), 0)
+		g.bricks = append(g.bricks, brick)
+	}
+
+	xCoords := []int{4, 5, 6, 7, 8, 9, 10, 11, 12}
+	randomX := float64(xCoords[rand.Intn(len(xCoords))] * BlockWidth)
+	brick1 := NewBrick(randomX, BlockHeight)
+	brick2 := NewBrick(randomX, 2*BlockHeight)
+	g.bricks = append(g.bricks, brick1, brick2)
+}
 func NewPlayer(game *Game) *Player {
 	sprite := PlayerSprite
 
 	pos := Vector{
 		X: BlockWidth / 2,
-		Y: ScreenHeight - BlockWidth,
+		Y: GroundY,
 	}
 	return &Player{
 		game:     game,
@@ -108,12 +140,20 @@ func (p *Player) Update() {
 func (p *Player) Draw(screen *ebiten.Image) {
 	op := &ebiten.DrawImageOptions{}
 
-	scaleX := 0.25
-	scaleY := 0.25
+	scaleX := 0.35
+	scaleY := 0.35
 	op.GeoM.Scale(scaleX, scaleY)
 	op.GeoM.Translate(p.position.X, p.position.Y)
 
 	screen.DrawImage(p.sprite, op)
+}
+
+func (b *Brick) Draw(screen *ebiten.Image) {
+	op := &ebiten.DrawImageOptions{}
+
+	op.GeoM.Translate(b.position.X, b.position.Y)
+
+	screen.DrawImage(b.sprite, op)
 }
 
 type Vector struct {
@@ -124,15 +164,22 @@ type Vector struct {
 type Game struct {
 	score  int
 	player *Player
+	bricks []*Brick
 }
 
 func (g *Game) Update() error {
 	g.player.Update()
+
 	return nil
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
 	g.player.Draw(screen)
+
+	for _, b := range g.bricks {
+		b.Draw(screen)
+	}
+
 	text.Draw(screen, fmt.Sprintf("%06d", g.score), ScoreFont, ScreenWidth/2-100, 50, color.White)
 }
 
@@ -149,6 +196,7 @@ func NewGame() *Game {
 
 func main() {
 	g := NewGame()
+	g.DrawGround()
 
 	err := ebiten.RunGame(g)
 	if err != nil {
